81c9ce12ca2376f88569b247850253c60f376632
Amos <178724675+W3BNEEK3@users.noreply.github.com>
2026-05-12 09:11:16 -0700

    same as last commit

---
+++ src/Identity/Domain/User/User.php
@@ -0,0 +1,52 @@
+<?php
+namespace Src\Identity\Domain\User;
+
+use Src\Shared\Domain\AggregateRoot;
+
+final class User extends AggregateRoot
+{
+    public function __construct(
+        private readonly UserId    $userId,
+        private string             $fullname,
+        private string             $email,
+        private string             $passwordHash
+    ){}
+
+    public function register(
+        UserId      $id,
+        string      $fullname,
+        string      $email,
+        string      $passwordHash
+    ): self {
+        $user = new self($id, $fullname, $email, $passwordHash);
+
+        $user->recordEvent(
+            new UserRegistered(
+                userId: (string) $id,
+                fullname: $fullname,
+                email: $email
+            )
+        );
+
+        return $user;
+    }
+    
+    public static function reconstitute(
+        UserId      $id,
+        string      $fullname,
+        string      $email,
+        string      $passwordHash
+    ): self {
+        return new self($id, $fullname, $email, $passwordHash);
+    }
+
+    public function id(): string
+    {
+        return (string) $this->userId;
+    }
+
+    public function userId(): UserId {return $this->userId};
+    public function fullname(): string {return $this->fullname};
+    public function email(): string {return $this->email};
+    public function passwordHash(): string {return $this->passwordHash};
+}
