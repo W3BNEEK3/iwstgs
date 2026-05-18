2e2a3783254a2aa3988532f8d871ee756fae4660
Amos <178724675+W3BNEEK3@users.noreply.github.com>
2026-05-18 04:04:46 -0700

    finsihed the EnrolAsLearnerHandler.php

--- src/SimExecution/Application/Command/EnrolAsLearner/EnrolAsLearnerHandler.php
+++ src/SimExecution/Application/Command/EnrolAsLearner/EnrolAsLearnerHandler.php
@@ -12,7 +12,7 @@ final class EnrolAsLearnerHandler
 {
     public function __construct(
         private readonly LearnerRepository $repository,
-        private readonly UuidGenerator $uuidGenerator    
+        private readonly UuidGenerator     $uuidGenerator    
     ){}
             
             public function handle(EnrolAsLearnerCommand $command)
@@ -22,9 +22,17 @@ final class EnrolAsLearnerHandler
             }
             
             Learner::enrol(
-               id:   LearnerId::fromString($this->uuidGenerator),
-               userId: $command->userId,
-               
+               id:              LearnerId::fromString($this->uuidGenerator),
+               userId:          $command->userId,
+               entryCategory:   Entrycategory::from($command->entryCategory),
+               yearsExperience: $command->yearsExperience,
+               organisationId:      $command->organisationId
             );
+
+            $this->repository->save($learner);
+
+            foreach($learner->releaseEvents() as $event){
+                event($event);
+            }
         }            
 }
